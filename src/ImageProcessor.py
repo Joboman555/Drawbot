1646acd9b2353733a9146110531de51c38dd56b3
Jonah Spear <Jonahspear512@gmail.com>
2017-04-27 11:30:52 -0400

    Make ImageProcessor generate points translated by neatos starting position

--- src/ImageProcessor.py
+++ src/ImageProcessor.py
@@ -6,20 +6,53 @@ from __future__ import division
 import rospy
 import cv2
 from geometry_msgs.msg import Point
+from nav_msgs.msg import Odometry
 from drawbot.srv import GetWaypoints, GetWaypointsResponse
 import numpy as np
 import sys
+import tf
 
 
 class ImageProcessor(object):
     def __init__(self, path):
         super(ImageProcessor, self).__init__() 
         rospy.init_node('ImageProcessorServer')
-        self.IMAGE = self.preprocess(path, final_size=(5, 5))
+
+        self.starting_position = None
+        self.starting_orientation = None
+        self.got_first_odom_msg = False
+
+        rospy.Subscriber('/odom', Odometry, self.update_odometry)
+        IMAGE = self.preprocess(path, final_size=(3, 3))
         s = rospy.Service('get_waypoints', GetWaypoints, self.handle_get_waypoints)
         print "Ready to give waypoints"
-        print self.IMAGE
+        print IMAGE
+        r = rospy.Rate(50)
+        while not self.got_first_odom_msg:
+            r.sleep()
+        self.bl_coords = self.generate_base_link_points(IMAGE, (0.5,0.5))
+        self.odom_coords = self.generate_odom_points(IMAGE, (0.5,0.5))
+        print self.starting_position
+        print self.starting_orientation
         rospy.spin()
+
+    def update_odometry(self, msg):
+        if not self.got_first_odom_msg:
+            pos = msg.pose.pose.position
+            self.starting_position = np.array([pos.x, pos.y, pos.z])
+            
+            current_quat = msg.pose.pose.orientation
+            self.starting_orientation = self.convert_to_euler(current_quat.x, current_quat.y, current_quat.z, current_quat.w)
+            
+            self.got_first_odom_msg = True
+
+    def convert_to_euler(self, x, y, z, w):
+        quaternion = (x, y, z, w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        roll = euler[0]
+        pitch = euler[1]
+        yaw = euler[2]
+        return np.array([roll, pitch, yaw])
 
     def preprocess(self, image_path, final_size):
         """Takes in an image path, and returns a processed numpy array."""
@@ -38,23 +71,36 @@ class ImageProcessor(object):
         scaled = coords * np.array([x_scale, y_scale]) / maxs
         return scaled
 
-    def generate_points(self, binary_image, (x_scale, y_scale)):
+    def generate_base_link_points(self, binary_image, (x_scale, y_scale)):
         # Finds coordinates where the image is not 0
         x_y_coords = np.argwhere(binary_image).astype(float)
         scaled_x_y_coords = self.scale(x_y_coords, (x_scale, y_scale))
         zs = np.zeros((x_y_coords.shape[0], 1))
         # We need to append x, y, and zs together
         points_coords = np.hstack((scaled_x_y_coords, zs))
+        return points_coords
+
+    def generate_odom_points(self, binary_image, (x_scale, y_scale)):
+        points_coords = self.generate_base_link_points(binary_image, (x_scale, y_scale))
+        points_coords_transformed = points_coords + self.starting_position
+        return points_coords_transformed
+
+    def array_to_points(self, arr):
+        """Converts a nx3 array toa  list of Points."""
+        # The second dimension must be 3
+        assert arr.shape[1] == 3
         points = []
-        for row in points_coords:
+        for row in arr:
             point = Point(x=row[0], y=row[1], z=row[2])
             points.append(point)
         return points
 
     def handle_get_waypoints(self, req):
         print "Got request for position [%s]" % (req.neatoPosition)
-        points = self.generate_points(self.IMAGE, (3,3))
-        return GetWaypointsResponse(points)
+        
+        base_link_points = self.array_to_points(self.bl_coords)
+        odom_points = self.array_to_points(self.odom_coords)
+        return GetWaypointsResponse(odom_points)
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
